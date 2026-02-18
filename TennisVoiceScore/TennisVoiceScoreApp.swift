bb514558dc00e76c4c473600bb96aae18e49b6d0
Aviad <aviadmerlin@gmail.com>
2026-02-18 18:57:30 +0200

    Initial Commit

---
+++ TennisVoiceScore/TennisVoiceScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisVoiceScoreApp.swift
+//  TennisVoiceScore
+//
+//  Created by Aviad Merlin on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TennisVoiceScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
